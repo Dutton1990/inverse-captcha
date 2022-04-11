7185cf8d3d2faba40774d715541fcf43bb031325
Christian Dutton <Dutton1990@googlemail.com>
2022-04-11 12:10:17 +0100

    add comments and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+## HI SAM
+
+I will not cry (on screen at least) if you rip this apart :) 
+
+Refactored from by first solution, realised I'd overengineered it. 
+
+Muchas gracias 
--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const inverseCaptcha = (nums) => {
   let sum = 0;
 
+  // if first num matches last, add first num to sum
   if (nums[0] === nums[nums.length - 1]) {
     sum += nums[0];
   }
 
+  // loop through nums array by 2
   for (let i = 0; i < nums.length; i += 2) {
+    
+    // if num matches the next num, add num to sum
     if (nums[i] === nums[i + 1]) {
       sum += nums[i];
-    }
+    } 
+    
+    //if num matches previous num, add previous num to sum
     if (nums[i] === nums[i - 1]) {
       sum += nums[i - 1];
     }
